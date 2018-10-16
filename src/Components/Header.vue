986901811dd983b1d61a91ec0f807cda27b6a22a
Lopatovas <39686486+Lopatovas@users.noreply.github.com>
2018-10-16 21:38:53 +0300

    Temp

--- src/Components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <nav class="navbar navbar-dark bg-dark">
-    <router-link to="/" class = "navbar-brand">Homepage</router-link>
-    <router-link to="/postcreationpage" class="navbar-brand">Post creation page</router-link>
-</nav>
-</template>
-
-<script>
-</script>
