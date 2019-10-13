7bec99970c98f609bf0221c58ca75c5b63bff012
Lopatovas <lopatovas@gmail.com>
2019-10-13 19:26:57 +0300

    Array game

---
+++ precache-manifest.e49ed191e80b036d8321d8fcbc83fb92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5ab98d10a05d09b46c4c99a0c95eba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75ca2ce9a4b33e6d80bc",
+    "url": "/static/css/main.fe0ee15b.chunk.css"
+  },
+  {
+    "revision": "c83d9ea085e37917f183",
+    "url": "/static/js/2.629a1327.chunk.js"
+  },
+  {
+    "revision": "75ca2ce9a4b33e6d80bc",
+    "url": "/static/js/main.f601bf8c.chunk.js"
+  },
+  {
+    "revision": "e8d3dd43cef98731e73c",
+    "url": "/static/js/runtime-main.0894420a.js"
+  }
+]);
